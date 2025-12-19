4e63d98f3614cb654ae63e11a956f8ce87121ad3
rafasixteen <rafaelrego16@gmail.com>
2025-12-19 10:53:06 +0000

    added routes to / and /home since the index.php is the route handler and cannot be used an the / entry point for the home page

--- public/index.php
+++ public/index.php
@@ -5,8 +5,6 @@ ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
-// TODO: Add authorization middleware.
-
 require __DIR__ . '/../vendor/autoload.php';
 
 $router = new AltoRouter();
@@ -29,6 +27,14 @@ if (str_starts_with($_SERVER['REQUEST_URI'], '/api')) {
 		exit();
 	}
 }
+
+$router->map('GET', '/', function () {
+	require __DIR__ . '/../public/home/index.php';
+});
+
+$router->map('GET', '/home', function () {
+	require __DIR__ . '/../public/home/index.php';
+});
 
 // Sensor API routes
 
