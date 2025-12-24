dc3e11536a7a50c72d9fd50cf91bc99da037dbbf
DESKTOP-N2SOVPU\rafae <rafaelrego16@gmail.com>
2025-12-24 09:34:15 +0000

    using a single css file to style the tables

--- public/doors.php
+++ public/doors.php
@@ -3,7 +3,7 @@ require __DIR__ . '/../vendor/autoload.php';
 
 $title = 'Doors';
 $view = __DIR__ . '/../pages/doors.php';
-$styles = ['/assets/css/doors.css'];
+$styles = ['/assets/css/table.css'];
 $scripts = ['https://code.jquery.com/jquery-3.7.1.min.js', '/assets/js/doors.js', '/assets/js/utils/format-iso.js'];
 
 require __DIR__ . '/../layouts/auth.php';
